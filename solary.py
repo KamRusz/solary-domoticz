da8e688d68500980861b75a4bfda74b9d4899ed8
Kamil <kamilrusztyn@gmail.com>
2022-07-11 20:39:36 +0200

    first working serial answer

--- solary.py
+++ solary.py
@@ -11,12 +11,14 @@ ser = serial.Serial(
         timeout=1
     )
 znakodczyt = bytearray(b'\x01\x01\x02\x03\x0a\x0b\x0c')
-data = bytearray(b'\x81\x54\x01\x5A\x78\x78\x01\x78\x78\x78\x23')
+data = bytearray(b'\x8A\x54\x0A\x5A\x78\x78\x01\x78\x78\x78\x23')
 
-data[6] = znakodczyt[1]
+#data[6] = znakodczyt[1]
 print(data[6])
 print(data)
 ser.write(data)
 x=ser.read(11)
-
-print("x =",x)
+#for i in x:
+#    print(i)
+temp = (100*x[8]+x[9])/10
+print("temp =",temp,"st C")
